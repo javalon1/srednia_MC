7a40ffecfc7d620040f73c27b545c08a47dff91e
ciechann <emgasuk@gmail.com>
2019-05-12 15:57:09 +0100

    srednia

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,22 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.scene.transform.Scale;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj n liczb w ciagu: ");
+        int n = scan.nextInt();
+        int wynik = 0;
+        System.out.println("Podaj "+ n + " liczb ciagu");
+        for (int i = 0; i < n; i++) {
+            int a = scan.nextInt();
+            wynik = wynik + a;
+        }
+        System.out.println(wynik/n);
     }
 }
+
